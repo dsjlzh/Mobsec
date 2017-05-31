1fbe2b4fed89e439b3053ab8112c750df245fb80
Gerald <sundaywind2004@gmail.com>
2017-05-31 15:43:41 +0800

    Init layout

--- src/main/kotlin/com/gerald/mobsec/MainActivity.kt
+++ src/main/kotlin/com/gerald/mobsec/MainActivity.kt
@@ -2,11 +2,25 @@ package com.gerald.mobsec
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.*
+import android.text.InputType.TYPE_CLASS_TEXT
+import android.text.InputType.TYPE_TEXT_VARIATION_PASSWORD
+import android.view.Gravity
+import android.widget.Button
+import android.widget.EditText
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
+
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_scan.onClick {
+            toast("Scan start (fake)")
+        }
     }
 }
